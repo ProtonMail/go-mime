dd390330f32be71e260c0b71420b67cf186e649a
Kay Lukas <kay.lukas@gmail.com>
2018-09-05 13:48:02 +0200

    bugfix

--- parser.go
+++ parser.go
@@ -390,11 +390,11 @@ func (bc BodyCollector) Accept(partReader io.Reader, header textproto.MIMEHeader
 	return
 }
 
-func (bc BodyCollector) GetBody() string {
+func (bc BodyCollector) GetBody() (string, string) {
 	if bc.hasHtml {
-		return bc.htmlBodyBuffer.String()
+		return bc.htmlBodyBuffer.String(), "text/html"
 	} else {
-		return bc.plainBodyBuffer.String()
+		return bc.plainBodyBuffer.String(), "text/plain"
 	}
 }
 
