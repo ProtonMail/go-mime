6d23a8f3d21f0433f7208c082d7583cb69eb5943
Jakub <jakub@cuth.sk>
2019-02-25 11:07:21 +0100

    extend tests with SlimAPI corrections

--- encoding.go
+++ encoding.go
@@ -9,8 +9,8 @@ import (
 	"strings"
 
 	"encoding/base64"
+	htmlsets "golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/charmap"
 	"golang.org/x/text/transform"
 )
 
@@ -27,172 +27,20 @@ var wordDec = &mime.WordDecoder{
 	},
 }
 
+func getEncoding(charset string) (enc *encoding.Encoding, err error) {
+	enc, err = htmlsets.Lookup(charset)
+	return
+}
+
 func selectDecoder(charset string) (decoder *encoding.Decoder, err error) {
-	var enc encoding.Encoding
-	// all MIME charset with aliases can be found here https://www.iana.org/assignments/character-sets/character-sets.xhtml
-	switch strings.ToLower(charset) {
-	case "utf-8", // MIB 16
-		"utf8",
-		"csutf8",
-		"us-ascii", // MIB 3
-		"iso-ir-6",
-		"ansi_x3.4-1968",
-		"ansi_x3.4-1986",
-		"iso_646.irv:1991",
-		"iso646-us",
-		"us",
-		"ibm367",
-		"cp367",
-		"csascii",
-		"ascii",
-		"ks_c_5601-1987",   // MIB 36
-		"euc-kr",           // MIB 38
-		"iso-2022-jp",      // MIB 39
-		"ansi_x3.110-1983", // MIB 74
-		"gb2312",           // MIB 2025
-		"":                 // Nothing to do
-		return
-	case "utf7", "utf-7":
+	lcharset := strings.Trim(strings.ToLower(charset))
+	if strings.Contains("utf-7", lcharset) || strings.Contains("unicode-1-1-utf-7") {
 		return NewUtf7Decoder(), nil
-	case "iso-8859-1", // MIB 4
-		"iso8859-1",
-		"so-ir-100",
-		"iso_8859-1",
-		"latin1",
-		"l1",
-		"ibm819",
-		"cp819",
-		"csisolatin1":
-		enc = charmap.ISO8859_1
-	case "iso-8859-2", // MIB 5
-		"iso-ir-101",
-		"iso_8859-2",
-		"iso8859-2",
-		"latin2",
-		"l2",
-		"csisolatin2":
-		enc = charmap.ISO8859_2
-	case "iso-8859-3", // MIB 6
-		"iso-ir-109",
-		"iso_8859-3",
-		"latin3",
-		"l3",
-		"csisolatin3":
-		enc = charmap.ISO8859_3
-	case "iso-8859-4", // MIB 7
-		"iso-ir-110",
-		"iso_8859-4",
-		"latin4",
-		"l4",
-		"csisolatin4":
-		enc = charmap.ISO8859_4
-	case "iso-8859-5", // MIB 8
-		"iso-ir-144",
-		"iso_8859-5",
-		"cyrillic",
-		"csisolatincyrillic":
-		enc = charmap.ISO8859_5
-	case "iso-8859-6", // MIB 9
-		"iso-ir-127",
-		"iso_8859-6",
-		"ecma-114",
-		"asmo-708",
-		"arabic",
-		"csisolatinarabic":
-		enc = charmap.ISO8859_6
-	case "iso-8859-6e", // MIB 81
-		"csiso88596e",
-		"iso-8859-6-e":
-		enc = charmap.ISO8859_6E
-	case "iso-8859-6i", // MIB 82
-		"csiso88596i",
-		"iso-8859-6-i":
-		enc = charmap.ISO8859_6I
-	case "iso-8859-7", // MIB 10
-		"iso-ir-126",
-		"iso_8859-7",
-		"elot_928",
-		"ecma-118",
-		"greek",
-		"greek8",
-		"csisolatingreek":
-		enc = charmap.ISO8859_7
-	case "iso-8859-8", // MIB 11
-		"iso-ir-138",
-		"iso_8859-8",
-		"hebrew",
-		"csisolatinhebrew":
-		enc = charmap.ISO8859_8
-	case "iso-8859-8e", // MIB 84
-		"csiso88598e",
-		"iso-8859-8-e":
-		enc = charmap.ISO8859_8E
-	case "iso-8859-8i", // MIB 85
-		"csiso88598i",
-		"iso-8859-8-i":
-		enc = charmap.ISO8859_8I
-	case "iso-8859-10", // MIB 13
-		"iso-ir-157",
-		"l6",
-		"iso_8859-10:1992",
-		"csisolatin6",
-		"latin6":
-		enc = charmap.ISO8859_10
-	case "iso-8859-13", // MIB 109
-		"csiso885913":
-		enc = charmap.ISO8859_13
-	case "iso-8859-14", // MIB 110
-		"iso-ir-199",
-		"iso_8859-14:1998",
-		"iso_8859-14",
-		"latin8",
-		"iso-celtic",
-		"l8",
-		"csiso885914":
-		enc = charmap.ISO8859_14
-	case "iso-8859-15", // MIB 111
-		"iso_8859-15",
-		"latin-9",
-		"csiso885915",
-		"ISO8859-15":
-		enc = charmap.ISO8859_15
-	case "iso-8859-16", // MIB 112
-		"iso-ir-226",
-		"iso_8859-16:2001",
-		"iso_8859-16",
-		"latin10",
-		"l10",
-		"csiso885916":
-		enc = charmap.ISO8859_16
-	case "windows-874", "cswindows874": // MIB 2109
-		enc = charmap.Windows874
-	case "windows-1250", "cswindows1250": // MIB 2250
-		enc = charmap.Windows1250
-	case "windows-1251", "cswindows1251": // MIB 2251
-		enc = charmap.Windows1251
-	case "windows-1252", "cswindows1252", "cp1252", "3dwindows-1252": // MIB 2252
-		enc = charmap.Windows1252
-	case "windows-1253", "cswindows1253": // MIB 2253
-		enc = charmap.Windows1253
-	case "windows-1254", "cswindows1254": // MIB 2254
-		enc = charmap.Windows1254
-	case "windows-1255", "cswindows1255": // MIB 2255
-		enc = charmap.Windows1255
-	case "windows-1256", "cswindows1256": // MIB 2256
-		enc = charmap.Windows1256
-	case "windows-1257", "cswindows1257": // MIB 2257
-		enc = charmap.Windows1257
-	case "koi8-r", "cskoi8r": // MIB 2084
-		enc = charmap.KOI8R
-	case "koi8-u", "cskoi8u": // MIB 2088
-		enc = charmap.KOI8U
-	case "macintosh", "mac", "csmacintosh": // MIB 2027
-		enc = charmap.Macintosh
-	default:
-		err = errors.New("Unsupported charset " + charset)
-		return
 	}
-	decoder = enc.NewDecoder()
+	enc, err := getEncoding(lcharset)
+	if err == nil && enc != nil {
+		decoder = enc.NewDecoder()
+	}
 	return
 }
 
--- encoding_test.go
+++ encoding_test.go
@@ -36,6 +36,208 @@ func TestDecodeHeader(t *testing.T) {
 	}
 }
 
+func TestGetEncoding(t *testing.T) {
+	// all MIME charset with aliases can be found here https://www.iana.org/assignments/character-sets/character-sets.xhtml
+	mimesets := map[string][]string{
+		"utf-8": []string{ // MIB 16
+			"utf8",
+			"csutf8",
+			"us-ascii", // MIB 3
+			"iso-ir-6",
+			"unicode-1-1-utf-8",
+			"iso-utf-8",
+			"utf8mb4",
+			"ansi_x3.4-1968",
+			"ansi_x3.4-1986",
+			"iso_646.irv:1991",
+			"iso646-us",
+			"us",
+			"ibm367",
+			"cp367",
+			"csascii",
+			"ascii",
+		},
+		"gbk": []string{
+			"gb2312", // MIB 2025
+		},
+		//"utf7": []string{"utf-7", "unicode-1-1-utf-7"},
+		"iso-8859-1": []string{ // MIB 4
+			"iso8859-1",
+			"so-ir-100",
+			"iso_8859-1",
+			"latin1",
+			"l1",
+			"ibm819",
+			"cp819",
+			"csisolatin1",
+		},
+		"iso-8859-2": []string{ // MIB 5
+			"iso-ir-101",
+			"iso_8859-2",
+			"iso8859-2",
+			"latin2",
+			"l2",
+			"csisolatin2",
+			"ibm852",
+		},
+		"iso-8859-3": []string{ // MIB 6
+			"iso-ir-109",
+			"iso_8859-3",
+			"latin3",
+			"l3",
+			"csisolatin3",
+		},
+		"iso-8859-4": []string{ // MIB 7
+			"iso-ir-110",
+			"iso_8859-4",
+			"latin4",
+			"l4",
+			"csisolatin4",
+		},
+		"iso-8859-5": []string{ // MIB 8
+			"iso-ir-144",
+			"iso_8859-5",
+			"cyrillic",
+			"csisolatincyrillic",
+		},
+		"iso-8859-6": []string{ // MIB 9
+			"iso-ir-127",
+			"iso_8859-6",
+			"ecma-114",
+			"asmo-708",
+			"arabic",
+			"csisolatinarabic"},
+		"iso-8859-6e": []string{ // MIB 81
+			"csiso88596e",
+			"iso-8859-6-e"},
+		"iso-8859-6i": []string{ // MIB 82
+			"csiso88596i",
+			"iso-8859-6-i"},
+		"iso-8859-7": []string{ // MIB 10
+			"iso-ir-126",
+			"iso_8859-7",
+			"elot_928",
+			"ecma-118",
+			"greek",
+			"greek8",
+			"csisolatingreek"},
+		"iso-8859-8": []string{ // MIB 11
+			"iso-ir-138",
+			"iso_8859-8",
+			"iso-8859-8-i", // Hebrew, the "i" means right-to-left, probably unnecessary with ISO cleaning above
+			"hebrew",
+			"csisolatinhebrew"},
+		"iso-8859-8e": []string{ // MIB 84
+			"csiso88598e",
+			"iso-8859-8-e"},
+		"iso-8859-8i": []string{ // MIB 85
+			"csiso88598i",
+			"iso-8859-8-i"},
+		"iso-8859-10": []string{ // MIB 13
+			"iso-ir-157",
+			"l6",
+			"iso_8859-10:1992",
+			"csisolatin6",
+			"latin6"},
+		"iso-8859-13": []string{ // MIB 109
+			"csiso885913"},
+		"iso-8859-14": []string{ // MIB 110
+			"iso-ir-199",
+			"iso_8859-14:1998",
+			"iso_8859-14",
+			"latin8",
+			"iso-celtic",
+			"l8",
+			"csiso885914"},
+		"iso-8859-15": []string{ // MIB 111
+			"iso_8859-15",
+			"latin-9",
+			"csiso885915",
+			"ISO8859-15"},
+		"iso-8859-16": []string{ // MIB 112
+			"iso-ir-226",
+			"iso_8859-16:2001",
+			"iso_8859-16",
+			"latin10",
+			"l10",
+			"csiso885916",
+		},
+		"windows-874": []string{ // MIB 2109
+			"cswindows874",
+			"cp874",
+			"iso-8859-11",
+			"tis-620",
+		},
+		"windows-1250": []string{ // MIB 2250
+			"cswindows1250",
+			"cp1250",
+		},
+		"windows-1251": []string{ // MIB 2251
+			"cswindows1251",
+			"cp1251",
+		},
+		"windows-1252": []string{ // MIB 2252
+			"cswindows1252",
+			"cp1252",
+			"3dwindows-1252",
+			"we8mswin1252",
+		},
+		"windows-1253": []string{"cswindows1253", "cp1253"},        // MIB 2253
+		"windows-1254": []string{"cswindows1254", "cp1254"},        // MIB 2254
+		"windows-1255": []string{"cswindows1255", "cp1255"},        // MIB 2255
+		"windows-1256": []string{"cswindows1256", "cp1256"},        // MIB 2256
+		"windows-1257": []string{"cswindows1257", "cp1257"},        // MIB 2257
+		"windows-1258": []string{"cswindows1258", "cp1258"},        // MIB 2257 "koi8-r":       []string{"cskoi8r"},            // MIB 2084
+		"koi8-u":       []string{"cskoi8u"},                        // MIB 2088
+		"macintosh":    []string{"mac", "macroman", "csmacintosh"}, // MIB 2027
+		"uhc": []string{ // Korea
+			"ks_c_5601-1987",
+			"ksc5601",
+			"cp949",
+		},
+		"big5": []string{
+			"big5",
+		},
+		"euckr": []string{
+			"euc-kr", // MIB 38
+			"ibm-euckr",
+		},
+		"euccn": []string{
+			"ibm-euccn",
+		},
+		"eucjp": []string{
+			"ibm-eucjp",
+		},
+		"iso2022jp": []string{
+			"csiso2022jp",
+			"iso-2022-jp", // MIB 39
+		},
+
+		"unkown": []string{
+			"cp850",
+			"cp858",            // "cp850"  Mostly correct except for the Euro sign
+			"ansi_x3.110-1983", // MIB 74 // utf 8 probably
+			"zht16mswin950",    // cp950
+			"cp950",
+		},
+	}
+
+	for expected, name := range mimesets {
+		enc, err := getEncoding(name)
+		if err != nil {
+			t.Errorf("Error while get encoding %v: %v", name, err)
+		}
+		canonical, err := Name(enc)
+		if err != nil {
+			t.Errorf("Error while get canonical %v: %v", name, err)
+		}
+		if canonical != expected {
+			t.Errorf("Expected %v but have %v", expected, canonical)
+		}
+	}
+
+}
+
 // sample text for UTF8 http://www.columbia.edu/~fdc/utf8/index.html
 func TestEncodeReader(t *testing.T) {
 	// define test data
@@ -105,6 +307,8 @@ func TestEncodeReader(t *testing.T) {
 			[]byte("He wes Leovena+APA-es sone -- li+APA-e him be Drihten.+A6QDtw- +A7MDuwPOA8MDwwOx- +A7wDvwPF- +A60DtAPJA8MDsQO9- +A7UDuwO7A7cDvQO5A7oDrg-. +BCcENQRABD0ENQQ7BDg- +BDgENwQxBEs- +BDcENAQ1BEEETA- +BDg- +BEIEMAQ8-,+BCcENQRABD0ENQQ7BDg- +BDgENwQxBEs- +BDcENAQ1BEEETA- +BDg- +BEIEMAQ8-,+C68LvguuC7ELvwuoC80LpA- +C64Lygu0C78LlQuzC78LsgvH- +C6QLrgu/C7QLzQuuC8oLtAu/- +C6oLywuyC80- +C4cLqQu/C6QLvgu1C6QLwQ- +C44LmQvNC5ULwQuuC80- +C5ULvgujC8sLrgvN-."),
 			"He wes Leovenaðes sone -- liðe him be Drihten.Τη γλώσσα μου έδωσαν ελληνική. Чернели избы здесь и там,Чернели избы здесь и там,யாமறிந்த மொழிகளிலே தமிழ்மொழி போல் இனிதாவது எங்கும் காணோம்.",
 		},
+
+		// add more from mutations of https://en.wikipedia.org/wiki/World_Wide_Web
 	}
 
 	// run tests
