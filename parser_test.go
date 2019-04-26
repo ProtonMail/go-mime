d637fccaa421ebb1b8c5cb4b3435f44b55470163
Jakub <jakub@cuth.sk>
2019-04-26 15:26:17 +0200

    testing wrong formating case

--- parser_test.go
+++ parser_test.go
@@ -12,8 +12,7 @@ import (
 	"testing"
 )
 
-func MinimalParse(mimeBody string) (readBody string, plainContents string, err error) {
-
+func minimalParse(mimeBody string) (readBody string, plainContents string, err error) {
 	mm, err := mail.ReadMessage(strings.NewReader(mimeBody))
 	if err != nil {
 		return
@@ -21,10 +20,14 @@ func MinimalParse(mimeBody string) (readBody string, plainContents string, err e
 
 	h := textproto.MIMEHeader(mm.Header)
 	mmBodyData, err := ioutil.ReadAll(mm.Body)
+	if err != nil {
+		return
+	}
 
 	printAccepter := NewMIMEPrinter()
 	plainTextCollector := NewPlainTextCollector(printAccepter)
-	err = VisitAll(bytes.NewReader(mmBodyData), h, plainTextCollector)
+	visitor := NewMimeVisitor(plainTextCollector)
+	err = VisitAll(bytes.NewReader(mmBodyData), h, visitor)
 
 	readBody = printAccepter.String()
 	plainContents = plainTextCollector.GetPlainText()
@@ -32,8 +35,7 @@ func MinimalParse(mimeBody string) (readBody string, plainContents string, err e
 	return readBody, plainContents, err
 }
 
-func AndroidParse(mimeBody string) (body, headers string, atts, attHeaders []string, err error) {
-
+func androidParse(mimeBody string) (body, headers string, atts, attHeaders []string, err error) {
 	mm, err := mail.ReadMessage(strings.NewReader(mimeBody))
 	if err != nil {
 		return
@@ -54,6 +56,96 @@ func AndroidParse(mimeBody string) (body, headers string, atts, attHeaders []str
 	attHeaders = attachmentsCollector.GetAttHeaders()
 
 	return
+}
+
+func TestParseBoundaryIsEmpty(t *testing.T) {
+	testMessage :=
+		`Date: Sun, 10 Mar 2019 11:10:06 -0600
+In-Reply-To: <abcbase64@protonmail.com>
+X-Original-To: enterprise@protonmail.com
+References: <abc64@unicoderns.com> <abc63@protonmail.com> <abc64@protonmail.com> <abc65@mail.gmail.com> <abc66@protonmail.com>
+To: "ProtonMail" <enterprise@protonmail.com>
+X-Pm-Origin: external
+Delivered-To: enterprise@protonmail.com
+Content-Type: multipart/mixed; boundary=ac7e36bd45425e70b4dab2128f34172e4dc3f9ff2eeb47e909267d4252794ec7
+Reply-To: XYZ <xyz@xyz.com>
+Mime-Version: 1.0
+Subject: Encrypted Message
+Return-Path: <xyz@xyz.com>
+From: XYZ <xyz@xyz.com>
+X-Pm-Conversationid-Id: gNX9bDPLmBgFZ-C3Tdlb628cas1Xl0m4dql5nsWzQAEI-WQv0ytfwPR4-PWELEK0_87XuFOgetc239Y0pjPYHQ==
+X-Pm-Date: Sun, 10 Mar 2019 18:10:06 +0100
+Message-Id: <68c11e46-e611-d9e4-edc1-5ec96bac77cc@unicoderns.com>
+X-Pm-Transfer-Encryption: TLSv1.2 with cipher ECDHE-RSA-AES256-GCM-SHA384 (256/256 bits)
+X-Pm-External-Id: <68c11e46-e611-d9e4-edc1-5ec96bac77cc@unicoderns.com>
+X-Pm-Internal-Id: _iJ8ETxcqXTSK8IzCn0qFpMUTwvRf-xJUtldRA1f6yHdmXjXzKleG3F_NLjZL3FvIWVHoItTxOuuVXcukwwW3g==
+Openpgp: preference=signencrypt
+User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Thunderbird/60.4.0
+X-Pm-Content-Encryption: end-to-end
+
+--ac7e36bd45425e70b4dab2128f34172e4dc3f9ff2eeb47e909267d4252794ec7
+Content-Disposition: inline
+Content-Transfer-Encoding: quoted-printable
+Content-Type: multipart/mixed; charset=utf-8
+
+Content-Type: multipart/mixed; boundary="xnAIW3Turb9YQZ2rXc2ZGZH45WepHIZyy";
+ protected-headers="v1"
+From: XYZ <xyz@xyz.com>
+To: "ProtonMail" <enterprise@protonmail.com>
+Subject: Encrypted Message
+Message-ID: <68c11e46-e611-d9e4-edc1-5ec96bac77cc@unicoderns.com>
+References: <abc64@unicoderns.com> <abc63@protonmail.com> <abc64@protonmail.com> <abc65@mail.gmail.com> <abc66@protonmail.com>
+In-Reply-To: <abcbase64@protonmail.com>
+
+--xnAIW3Turb9YQZ2rXc2ZGZH45WepHIZyy
+Content-Type: text/rfc822-headers; protected-headers="v1"
+Content-Disposition: inline
+
+From: XYZ <xyz@xyz.com>
+To: ProtonMail <enterprise@protonmail.com>
+Subject: Re: Encrypted Message
+
+--xnAIW3Turb9YQZ2rXc2ZGZH45WepHIZyy
+Content-Type: multipart/alternative;
+ boundary="------------F9E5AA6D49692F51484075E3"
+Content-Language: en-US
+
+This is a multi-part message in MIME format.
+--------------F9E5AA6D49692F51484075E3
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: quoted-printable
+
+Hi ...
+
+--------------F9E5AA6D49692F51484075E3
+Content-Type: text/html; charset=utf-8
+Content-Transfer-Encoding: quoted-printable
+
+<html>
+  <head>
+  </head>
+  <body text=3D"#000000" bgcolor=3D"#FFFFFF">
+    <p>Hi ..  </p>
+  </body>
+</html>
+
+--------------F9E5AA6D49692F51484075E3--
+
+--xnAIW3Turb9YQZ2rXc2ZGZH45WepHIZyy--
+
+--ac7e36bd45425e70b4dab2128f34172e4dc3f9ff2eeb47e909267d4252794ec7--
+
+
+`
+
+	body, content, err := minimalParse(testMessage)
+	if err == nil {
+		t.Fatal("should have error but is", err)
+	}
+	t.Log("==BODY==")
+	t.Log(body)
+	t.Log("==CONTENT==")
+	t.Log(content)
 }
 
 func TestParse(t *testing.T) {
@@ -86,9 +178,9 @@ this is the attachment text
 
 
 `
-	body, heads, att, attHeads, err := AndroidParse(testMessage)
+	body, heads, att, attHeads, err := androidParse(testMessage)
 	if err != nil {
-		t.Error(err)
+		t.Error("parse error", err)
 	}
 
 	fmt.Println("==BODY:")
